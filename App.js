15b88b44bafbc4b8894d868042b8db33f822508f
Tushar Pawar <tushharpawar14@gmail.com>
2023-07-25 22:29:31 +0530

    App(Start)

---
+++ App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import {Header,Footer} from './Header';
+
+function App() {
+      return(
+        <div className='App'>
+          <Header/>
+          <Footer/>
+          <Header/>
+          <Footer/>
+        </div>
+      )
+}
+ 
+export default App;
