bdc34485fc3cb9e341a152ba1a5f29a93691f324
Tushar Pawar <tushharpawar14@gmail.com>
2023-07-25 22:30:38 +0530

    Header(demo component)

---
+++ Header.jsx
@@ -0,0 +1,16 @@
+const Header=()=>{
+    return(
+       <div>
+        <h1>Header</h1>
+        <p>Hello,I'm part of header.</p>
+       </div>
+    )
+}
+
+function Footer(){
+    return(
+        <h1>Footer</h1>
+    )
+}
+
+export {Header,Footer};
