5dcd42a3949c38fe3d0b4c41808a802656dd07fa
Tushar Pawar <tushharpawar14@gmail.com>
2023-08-07 22:40:11 +0530

    Router index & dynamic

---
+++ R-in-dynamic/App.js
@@ -0,0 +1,43 @@
+import React,{useState,useEffect} from 'react';
+import './App.css'; 
+import Header from './Header';
+import Home from './Home';
+import Contact from './contact';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom";
+import About from './About';
+import Error from './Error';
+import Insta from './Insta';
+import Email from './Email';
+import UserDetails from './UserDetails';
+
+const App = () => {
+     return (
+      <Router>
+        <div>
+        <Header/>
+
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/app/:userId' element={<UserDetails/>}/>
+          
+            
+          <Route path='/About' element={<About/>}/>
+            
+          <Route path='/contact' element={<Contact/>}>
+          <Route index element={<Insta/>}/>
+            <Route path='Insta' element={<Insta/>}/>
+            <Route path='Email' element={<Email/>}/>
+          </Route>
+          <Route path='*' element={<Error/>}/>
+            
+        </Routes>
+        </div>
+      </Router>
+     ) 
+}
+
+export default App;
