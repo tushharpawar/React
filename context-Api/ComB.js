b2c82d9857ce0aa164640a6ccd3a31b0919d721a
Tushar Pawar <tushharpawar14@gmail.com>
2023-08-08 22:02:35 +0530

    Component B

---
+++ context-Api/ComB.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import ComC from './ComC'
+
+const ComB = ({data}) => {
+  return (
+    <>
+        <div>ComB</div>
+        <ComC data={data}/>
+    </>
+  )
+}
+
+export default ComB
