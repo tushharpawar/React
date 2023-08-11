5592f85c8cad1bd22bbb5f70e33024a46e327fde
Tushar Pawar <tushharpawar14@gmail.com>
2023-08-11 23:02:25 +0530

    App file

--- p1/App.js
+++ p1/App.js
@@ -1,10 +1,14 @@
 import "./App.css";
 import Navbar from "./Navbar";
-
+import TextForm from "./TextForm";
 const App = () => {
   return (
-    
+  <>
     <Navbar title="Blog" aboutText="About Us"></Navbar>
+    <div className="container my-3">
+    <TextForm heading="Enter the text to analyse below"></TextForm>
+    </div>
+  </>
   );
 };
 
