02a548044490e3806509de9bee2123f922fc45cc
Tushar Pawar <tushharpawar14@gmail.com>
2023-08-12 22:00:15 +0530

    Updated Text Form

--- p1/TextForm.js
+++ p1/TextForm.js
@@ -8,11 +8,13 @@ export default function TextForm(props) {
         // console.log("HEllo");
         let newText=text.toUpperCase();
         setText(newText);
+        props.showAlert("Converted to Upper Case","success")
     }
     const handleLowClick=()=>{
         // console.log("HEllo");
         let newText=text.toLowerCase();
         setText(newText);
+        props.showAlert("Converted to Lower Case","success");
     }
     const handleClearClick=()=>{
         // console.log("HEllo");
@@ -21,25 +23,26 @@ export default function TextForm(props) {
     }
     const handleOnChange=(event)=>{
         setText(event.target.value);
+        
     }
 
   return (
    <>
-     <div className='container'>
+     <div className='container' style={{color:props.mode==='dark'?'white':'black'}}>
     <h1>{props.heading} </h1>
        <div className="mb-3">
-         <textarea className="form-control" value={text} id="myBox" rows="8" onChange={handleOnChange}></textarea>
+         <textarea className="form-control" style={{backgroundColor:props.mode==='dark'?'#212325':'white',color:props.mode==='dark'?'white':'black'}} value={text} id="myBox" rows="8" onChange={handleOnChange}></textarea>
        </div>
        <button className='btn btn-primary mx-2' onClick={handleUpClick}>Convert to Uppercase</button>
        <button className='btn btn-primary' onClick={handleLowClick}>Convert to Uppercase</button>
        <button className='btn btn-primary mx-2' onClick={handleClearClick}>Clear</button>
     </div>
-    <div className="container my-3">
+    <div className="container my-3" style={{color:props.mode==='dark'?'white':'black'}}>
         <h2>Your Text Summary</h2>
         <p>{text.split(" ").length} words and {text.length} characters</p>
         <p>{0.008*text.split(" ").length} Minutes to read</p>
         <h2>Preview</h2>
-        <p>{text}</p>
+        <p>{text.length>0?text:"Enter Something"}</p>
     </div>
    </>
   )
