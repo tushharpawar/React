b310f46420f2c62bb17a1186532cb354da605f61
Tushar Pawar <tushharpawar14@gmail.com>
2023-08-13 22:20:48 +0530

    Apply

---
+++ Placement-project/Apply.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Apply() {
+  
+
+  return (
+    <>
+      <div className='font-semibold text-sm md:text-xl pr-4 cursor-pointer'>Apply
+            <div className='w-10 md:w-[55px] h-1 md:h-1 border-b-4 border-blue-800 mt-1 rounded-10%] hidden md:rounded-lg md:mt-4 mb-10' id='bar'></div>
+      </div>
+    </>
+  )
+}
