9b5dbd48a60b4a93e7dba6ed1e638fe2a6040e03
Tushar Pawar <tushharpawar14@gmail.com>
2023-08-13 22:18:32 +0530

    Body

---
+++ Placement-project/body.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from './Header'
+import Form from './Form'
+
+
+export default function Body() {
+  return (
+    <div className='h-[100vh] w-[100vw]'>
+       <Header/>
+       <Form/>
+    </div>
+  )
+}
