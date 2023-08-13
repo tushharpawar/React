08767e814527833c47498cd1410c322aa4bf7d7e
Tushar Pawar <tushharpawar14@gmail.com>
2023-08-13 22:21:07 +0530

    Applied

---
+++ Placement-project/Applied.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function Applied() {
+   
+
+  return (
+    <>
+        <div className='w-auto h-auto font-semibold text-sm md:text-xl cursor-pointer'><a href="Table.js">Applied</a>
+            <div className='w-13 md:w-[75px] h-1 border-b-4 border-blue-800 mt-1 rounded-xs md:rounded-lg md:mt-4 mb-10 ' id='bar'></div>
+        </div>
+    </>
+  )
+}
