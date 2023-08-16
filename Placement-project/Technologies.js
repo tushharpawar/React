0275f95da3f861ceba808d0b2ff74509b45241da
Tushar Pawar <tushharpawar14@gmail.com>
2023-08-16 21:03:54 +0530

    Technologies

---
+++ Placement-project/Technologies.js
@@ -0,0 +1,33 @@
+import * as React from 'react';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+
+export default function Technologies() {
+  const [age, setAge] = React.useState('');
+
+  const handleChange = (event) => {
+    setAge(event.target.value);
+  };
+
+  return (
+    <div>
+      <FormControl sx={{ m: 1, minWidth: 80 }}>
+        <InputLabel id="demo-simple-select-autowidth-label"></InputLabel>
+        <Select
+          labelId="demo-simple-select-autowidth-label"
+          id="demo-simple-select-autowidth"
+          value={age}
+          onChange={handleChange}
+          autoWidth
+          
+        >
+          <MenuItem value={"react"}>React</MenuItem>
+          <MenuItem value={"flutter"}>Flutter</MenuItem>
+          
+        </Select>
+      </FormControl>
+    </div>
+  );
+}
