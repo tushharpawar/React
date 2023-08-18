f4510b3e5bf2fe123b60efa99572010f8eba9bc1
Tushar Pawar <tushharpawar14@gmail.com>
2023-08-18 23:12:52 +0530

    NewsItem.js

--- p2/components/NewsItem.js
+++ p2/components/NewsItem.js
@@ -1,17 +1,24 @@
 import React, { Component } from 'react'
 
 export class NewsItem extends Component {
+
+  
+
   render() {
-   let {title,description} =this.props
+   let {title,description,imageUrl,newsUrl} =this.props
+
     return (
+      <div className='my-3'>
         <div className="card" style={{width:"18rem"}}>
-        <img src="..." className="card-img-top" alt="..."/>
+        <img src={!imageUrl?"https://www.aljazeera.com/wp-content/uploads/2023/08/33P27LA-highres-1692267654.jpg?resize=1920%2C1440":imageUrl} className="card-img-top" alt="..."/>
         <div className="card-body">
-          <h5 className="card-title">{title}</h5>
-          <p className="card-text">{description}</p>
-          <a href="/" className="btn btn-primary">Go somewhere</a>
+          <h3 className="card-title">{title}...</h3>
+          <p className="card-text">{description}...</p>
+        <a rel="noreference" href={newsUrl} target='blank' className="btn btn-sm btn-dark my-1">Read More</a>
         </div>
       </div>
+      </div>
+        
     )
   }
 }
