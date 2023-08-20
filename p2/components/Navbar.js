4e0f5ceebf7e76e0a172fe422941545579572271
Tushar Pawar <tushharpawar14@gmail.com>
2023-08-20 20:35:17 +0530

    Navbar

--- p2/components/Navbar.js
+++ p2/components/Navbar.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
 
 export class Navbar extends Component {
   static propTypes = {}
@@ -8,18 +9,22 @@ export class Navbar extends Component {
       
     <nav className="navbar navbar-expand-lg bg-body-tertiary">
       <div className="container-fluid">
-      <a className="navbar-brand" href='/'>NewsCrow</a>
+      <Link className="navbar-brand" to='/'>NewsCrow</Link>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
         <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href='/'>Home</a>
+          <Link className="nav-link active" aria-current="page" to='/'>Home</Link>
         </li>
-        <li className="nav-item">
-          <a className="nav-link" href='/'>About</a>
-        </li>
+        <li className="nav-item" ><Link to='/business'>Business</Link></li>
+        <li className="nav-item" ><Link to='/entertainent'>Entertainent</Link></li>
+        <li className="nav-item" ><Link to='/general'>General</Link></li>
+        <li className="nav-item" ><Link to='/health'>Health</Link></li>
+        <li className="nav-item" ><Link to='/science'>Science</Link></li>
+        <li className="nav-item" ><Link to='/sports'>Sports</Link></li>
+        <li className="nav-item" ><Link to='/technology'>Technology</Link></li>
       </ul>
     
     </div>
