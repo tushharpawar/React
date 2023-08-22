91988c36312ad3546f68ffda680dda2b1cb48da0
Tushar Pawar <tushharpawar14@gmail.com>
2023-08-22 23:21:23 +0530

    News

--- p2/components/News.js
+++ p2/components/News.js
@@ -2,13 +2,15 @@ import React, { Component } from "react";
 import NewsItem from "./NewsItem";
 import Spinner from "./Spinner";
 import PropTypes from "prop-types";
+import InfiniteScroll from "react-infinite-scroll-component";
+
 
 export class News extends Component {
   static defalutProps = {
     country: "in",
     pageSize: 8,
     category: "general",
-  }
+  };
 
   static propTypes = {
     country: PropTypes.string,
@@ -16,25 +18,31 @@ export class News extends Component {
     category: PropTypes.string,
   };
 
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
       articles: [],
       loading: false,
       page: 1,
+      totalResults:0
     };
+    document.title = `${this.props.category}-NewsCrow`;
   }
 
   async updateNews() {
+    this.props.setProgress(10)
     const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=a2b6080a3dae4b2d98b41bbe7beebd47&page=${this.state.page}&pageSize=${this.props.pageSize}`;
     this.setState({ loading: true });
     let data = await fetch(url);
+    this.props.setProgress(30)
     let parseData = await data.json();
+    this.props.setProgress(70)
     this.setState({
       articles: parseData.articles,
       totalResults: parseData.totalResults,
       loading: false,
     });
+    this.props.setProgress(100)
   }
 
   async componentDidMount() {
@@ -51,19 +59,38 @@ export class News extends Component {
     this.updateNews();
   };
 
+  fetchMoreData = async() => {
+       this.setState({page:this.state.page+1})
+       const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=a2b6080a3dae4b2d98b41bbe7beebd47&page=${this.state.page}&pageSize=${this.props.pageSize}`;
+    let data = await fetch(url);
+    let parseData = await data.json();
+    this.setState({
+      articles: this.state.articles.concat(parseData.articles),
+      totalResults: parseData.totalResults,
+      loading: false,
+    });
+       
+  };
+
   render() {
     return (
-      <div className="container my-3">
+      <>
         <h1
           className="text-center"
           style={{ fontSize: "30px", fontWeight: "700" }}
         >
-          NewsCrow-Top Headlines
+          NewsCrow-Top Headlines on {this.props.category}
         </h1>
-        {this.state.loading && <Spinner />}
-        <div className="row">
-          {!this.state.loading &&
-            this.state.articles.map((element) => {
+        {/* {this.state.loading && <Spinner />} */}
+        <InfiniteScroll
+          dataLength={this.state.articles.length}
+          next={this.fetchMoreData}
+          hasMore={this.state.articles.length !== this.state.totalResults}
+          loader={<Spinner/>}
+        >
+          <div className="container">
+          <div className="row">
+            {this.state.articles.map((element) => {
               return (
                 <div className="col-md-4" key={element.url}>
                   <NewsItem
@@ -82,31 +109,11 @@ export class News extends Component {
                 </div>
               );
             })}
-        </div>
-        <div className="container">
-          <div className="d-flex justify-content-between">
-            <button
-              disabled={this.state.page <= 1}
-              type="button"
-              className="btn btn-outline-primary"
-              onClick={this.handlePrevClick}
-            >
-              &larr; Previous
-            </button>
-            <button
-              disabled={
-                this.state.page + 1 >
-                Math.ceil(this.state.totalResults / this.props.pageSize)
-              }
-              type="button"
-              className="btn btn-outline-primary"
-              onClick={this.handleNextClick}
-            >
-              Next &rarr;
-            </button>
           </div>
-        </div>
-      </div>
+          </div>
+        </InfiniteScroll>
+        </>
+      
     );
   }
 }
