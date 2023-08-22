1499d7e5cc207a30a6a72b0fae08e1de6c6bf744
Tushar Pawar <tushharpawar14@gmail.com>
2023-08-22 23:21:07 +0530

    Navbar

--- p2/components/Navbar.js
+++ p2/components/Navbar.js
@@ -8,8 +8,8 @@ export class Navbar extends Component {
     return (
       
     <nav className="navbar navbar-expand-lg bg-body-tertiary">
-      <div className="container-fluid">
-      <Link className="navbar-brand" to='/'>NewsCrow</Link>
+      <div className="container-fluid" >
+      <Link className="navbar-brand" to='/' style={{fontWeight:"600"}}>NewsCrow</Link>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
