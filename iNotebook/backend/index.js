9651a9dd297fa0f81009f0ab822ef2940bbadcb2
Tushar Pawar <tushharpawar14@gmail.com>
2023-08-25 23:48:35 +0530

    index

--- iNotebook/backend/index.js
+++ iNotebook/backend/index.js
@@ -1,15 +1,16 @@
-const connectToMongo =require('./db')
-const express = require('express')
+const connectToMongo = require("./db");
+const express = require("express");
 
 connectToMongo();
 
-const app = express()
-const port = 3000
+const app = express();
+const port = 5000;
 
-app.get('/', (req, res) => {
-  res.send('Hello Tushar!')
-})
+app.use(express.json());
+
+app.use("/api/auth", require("./routes/auth"));
+// app.use('/api/auth',require('./routes/notes'))
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
