776bd60bb2aa6638da074856dcdd07ec4eb0484c
Tushar Pawar <tushharpawar14@gmail.com>
2023-08-27 22:23:47 +0530

    auth

--- iNotebook/backend/routes/auth.js
+++ iNotebook/backend/routes/auth.js
@@ -5,10 +5,10 @@ const { body, validationResult } = require("express-validator");
 const bcrypt = require('bcryptjs');
 const { genSalt } = require("bcryptjs");
 var jwt = require('jsonwebtoken');
-
+var fetchuser=require('../middleware/fetchuser')
 
 const JWT_SECRET="Tusharisagoodb$oy"
-//create a User using :POST "/api/auth/createuser".No login required
+//Route:1  create a User using :POST "/api/auth/createuser".No login required
 router.post(
   "/createuser",
   [
@@ -57,5 +57,58 @@ router.post(
     }
   }
 );
+
+//Route:2 Authenticate the user login
+
+router.post(
+  "/login",
+  [
+    body("email", "Enter valid email").isEmail(),
+    body("password", "Password can not be blank").exists(),
+  ],
+  async (req, res) =>{
+
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+  const{email,password}=req.body;
+  try{
+      let user=await User.findOne({email})
+      if(!user){
+        return res.status(400).json({error:"Pls try to login with correct credentials"});
+      }
+      const passwordCompare=await bcrypt.compare(password,user.password);
+      if(!passwordCompare){
+        return res.status(400).json({error:"Pls try to login with correct credentials"});
+      }
+      const data={
+        user:{
+          id:user.id
+        }
+      }
+      const authToken=jwt.sign(data,JWT_SECRET)
+      res.json({authToken});
+  }catch (error) {
+    console.error(error.message);
+    res.status(500).send("Internal Server Error:(");
+  }
+})
+
+// Route :3 Get user details.Login required!
+
+router.post(
+  "/getuser",fetchuser, async (req, res) =>{
+    
+    try{
+      let userId=req.user.id;
+      const user=await User.findById(userId).select(".password")
+      res.send(user)
+    }catch (error) {
+      console.error(error.message);
+      res.status(500).send("Internal Server Error:(");
+    }
+  })
 
 module.exports = router;
