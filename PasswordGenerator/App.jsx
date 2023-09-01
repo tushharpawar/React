ee30130ad17a64da29d3f90db9703c8a9e6f7cc9
Tushar Pawar <tushharpawar14@gmail.com>
2023-09-01 22:01:26 +0530

    Updated App.jsx

--- PasswordGenerator/App.jsx
+++ PasswordGenerator/App.jsx
@@ -1,10 +1,28 @@
-import { useState } from 'react'
+import { useState,useCallback } from 'react'
 import './App.css'
 
 function App() {
   const [lenght, setLenght] = useState(8);
   const [numberAllowed, setNumberAllowed] = useState(false);
   const [charAllowed, setCharAllowed] = useState(false);
+  const [password, setPassword] = useState("");
+
+  const passwordGenerator = useCallback(()=>{
+    let pass=""
+    let str="ABCDEFGHIJKLMNOPQRSTUWXYZabcdefghijklmnopqrstuvwxyz"
+
+    if(numberAllowed) str += "0123456789"
+    if(charAllowed) str += "!@#$%^&*()_-+={}[]|/.?"
+
+    for (let i = 1; i <= array.length; i++) {
+      let char = Math.floor(Math.random() * str.length+1)
+      pass =str.charAt(char)
+    }
+
+    setPassword(pass);
+
+  }, [lenght,numberAllowed,charAllowed,setPassword])
+
 
   return (
     <>
