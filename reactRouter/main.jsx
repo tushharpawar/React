a11cdd443ead37703ca18abdd8464523cff48c59
Tushar Pawar <tushharpawar14@gmail.com>
2023-09-08 21:46:12 +0530

    Create main.jsx

---
+++ reactRouter/main.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import Layout from './Layout'
+import Home from './components/Home/home'
+import About from './components/About/About'
+
+const router=createBrowserRouter([
+  {
+    path:"/",
+    element:<Layout/>,
+    children:[
+      {
+        path:"",
+        element:<Home/>
+      },
+      {
+        path:"about",
+        element:<About/>
+      }
+    ]
+
+}])
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router}/>
+  </React.StrictMode>,
+)
