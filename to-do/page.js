f7727a1659978bc987aa68a448f31ed0d333f980
Tushar Pawar <tushharpawar14@gmail.com>
2023-09-09 21:38:10 +0530

    Create page.js

---
+++ to-do/page.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <>
+      <h1 className='text-4xl font-bold bg-black text-white text-center p-5'>My To-Do List</h1>
+      <form>
+        <input type="text" className='text-2xl border-zinc-800 border-2 m-5' />
+      </form>
+    </>
+  )
+
+}
+
+export default page
