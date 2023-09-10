0d9bbb67cbe29d47b549dfbfa28eef792a8ce852
Tushar Pawar <tushharpawar14@gmail.com>
2023-09-10 22:03:11 +0530

    Update page.js

--- to-do/page.js
+++ to-do/page.js
@@ -5,7 +5,7 @@ const page = () => {
     <>
       <h1 className='text-4xl font-bold bg-black text-white text-center p-5'>My To-Do List</h1>
       <form>
-        <input type="text" className='text-2xl border-zinc-800 border-2 m-5' />
+        <input type="text" className='text-2xl border-zinc-800 border-2 m-5 px-4 py-2' />
       </form>
     </>
   )
