0e697981a666dff34f87b5131519b73dfd20986d
Tushar Pawar <tushharpawar14@gmail.com>
2023-09-13 23:21:02 +0530

    Create App.jsx

---
+++ ContextApi/p2/App.jsx
@@ -0,0 +1,55 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+import { ThemeProvider } from "./context/theme";
+import ThemeBtn from "./components/ThemeBtn";
+import Card from "./components/Card";
+
+function App() {
+   const[themeMode,setThemeMode]=useState("light");
+
+   const lightTheme=()=>{
+    setThemeMode("light");
+   }
+   const darkTheme=()=>{
+    setThemeMode("dark");
+   }
+   
+   useEffect(() => {
+    document.querySelector("html").classList.remove("light","dark");
+    document.querySelector("html").classList.add(themeMode)
+   }, [themeMode])
+   
+  return (
+    <ThemeProvider value={{themeMode,lightTheme,darkTheme}}>
+    <div className="flex flex-wrap min-h-screen items-center">
+      <div className="w-full">
+        <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+          <ThemeBtn/>
+        </div>
+
+        <div className="w-full max-w-sm mx-auto">
+          <Card/>
+        </div>
+      </div>
+    </div>
+    </ThemeProvider>
+  );
+}
+
+export default App;
+
+
+// pls notice this also:
+// /** @type {import('tailwindcss').Config} */
+// export default {
+//   content: [
+//     "./index.html",
+//     "./src/**/*.{js,ts,jsx,tsx}",
+//   ],
+//   darkMode:"class",
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// }
+
