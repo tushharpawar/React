c891ac701f9c817cc7b349920a14f9c95d349642
Tushar Pawar <tushharpawar14@gmail.com>
2023-09-15 23:27:18 +0530

    Create ToDoContext.js

---
+++ ContextApi/p3/context/ToDoContext.js
@@ -0,0 +1,19 @@
+import { createContext,useContext } from "react";
+
+export const ToDoContext=createContext({
+    todos:[{
+        id:1,
+        todo:"To Do",
+        compeleted:false,
+    }],
+    addToDo:(todo)=>{},
+    updateTodo:(id,todo)=>{},
+    deleteTodo:(id)=>{},
+    toggleCompelete:(id)=>{}
+})
+
+export const useToDo=()=>{
+   return useContext(ToDoContext)
+}
+
+export const ToDoProvider=ToDoContext.Provider;
