60076d190bddf3d1d50de3ca1aa7d541b5a33c03
Tushar Pawar <tushharpawar14@gmail.com>
2023-09-28 22:26:03 +0530

    Update App.jsx

--- Blog-Appwrite-Project/App.jsx
+++ Blog-Appwrite-Project/App.jsx
@@ -1,14 +1,38 @@
-
-import './App.css'
+import { useState,useEffect } from 'react'
+import { useDispatch } from 'react-redux'
+import authService from './appwrite/auth'
+import {login, logout} from './store/authSlice'
+import { Header,Footer } from './components/index'
 
 function App() {
 
-
-  return (
-    <>
-     <h1>Hello</h1>
-    </>
-  )
+  const [loading,setLoading] =useState(true)
+  const dispatch = useDispatch();
+  
+  useEffect(() => {
+  authService.getCurruntUser()  
+  .then((userData)=>{
+    if(userData){
+      dispatch(login({userData}))
+    }
+    else{
+      dispatch(logout())
+    }
+  })
+  .finally(()=>{setLoading(false)})
+  }, [])
+  
+  return !loading ? (
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+      <div className='w-full block'>
+        <Header/>
+        <main>
+          {/*Outlet*/}
+        </main>
+        <Footer/>
+      </div>
+    </div>
+  ):null
 }
 
 export default App
