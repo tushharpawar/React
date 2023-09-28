cbc2068aed483a0ea1bdf6eca4de4fc4164fecd6
Tushar Pawar <tushharpawar14@gmail.com>
2023-09-28 22:27:23 +0530

    Update authSlice.js

--- Blog-Appwrite-Project/store/authSlice.js
+++ Blog-Appwrite-Project/store/authSlice.js
@@ -1,5 +1,24 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const authSlice=createSlice({})
+const initialState={
+    status:false,
+    userData:null,
+}
 
-export default authSlice
+const authSlice=createSlice({
+       name:"auth",
+       initialState,
+       reducers:{
+              login:(state,action)=>{
+                state.status=true
+                state.userData=action.payload.userData
+              },
+              logout:(state)=>{
+                state.status=false
+                state.userData=null
+              }
+       }
+})
+
+export const {login,logout}=authSlice.actions;
+export default authSlice;
