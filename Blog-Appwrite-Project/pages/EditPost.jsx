9e0a18ebb5f6925e25a051f4b389740c6a6a2fb3
Tushar Pawar <tushharpawar14@gmail.com>
2023-10-02 22:39:53 +0530

    Create EditPost.jsx

---
+++ Blog-Appwrite-Project/pages/EditPost.jsx
@@ -0,0 +1,35 @@
+import { useState,useEffect } from "react"
+import { Container,PostForm } from "../components/index"
+import { useNavigate, useParams } from 'react-router-dom';
+
+import appwriteService from '../appwrite/config'
+
+const EditPost = () => {
+
+  const [post,setPosts]=useState([])
+    const {slug}=useParams()
+    const navigate=useNavigate()
+
+    useEffect(()=>{
+      if(slug){
+        appwriteService.getPost(slug).then((post)=>{
+          if(post){
+            setPosts(post)
+          }
+        })
+      }
+
+      else{
+        navigate('/')
+      }
+    },[slug,navigate])
+  return post ? (
+    <div className='py-8'>
+        <Container>
+            <PostForm post={post} />
+        </Container>
+    </div>
+  ):null
+}
+
+export default EditPost
