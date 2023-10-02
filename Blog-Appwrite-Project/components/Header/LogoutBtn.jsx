27f599dc98d9856007dcda13c962f2a66d840a3d
Tushar Pawar <tushharpawar14@gmail.com>
2023-10-02 22:38:21 +0530

    Update LogoutBtn.jsx

--- Blog-Appwrite-Project/components/Header/LogoutBtn.jsx
+++ Blog-Appwrite-Project/components/Header/LogoutBtn.jsx
@@ -1,5 +1,5 @@
 import { useDispatch } from 'react-redux'
-import {authService} from '../../appwrite/config'
+import authService from '../../appwrite/auth'
 import {logout} from '../../store/authSlice'
 
 const LogoutBtn = () => {
@@ -10,7 +10,8 @@ const LogoutBtn = () => {
         .then(()=>dispatch(logout()))
     }
   return (
-    <button className='inline-bock px-6 py-2 duration-200 hover:bg-blue-100 rounded-full'>Logout</button>
+    <button className='inline-bock px-6 py-2 duration-200 hover:bg-blue-100 rounded-full'
+    onClick={logoutHandler}>Logout</button>
   )
 }
 
