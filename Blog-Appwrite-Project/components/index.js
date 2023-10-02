f9258c1eec8844a4f76be76c5a9edf059d217c1d
Tushar Pawar <tushharpawar14@gmail.com>
2023-10-02 22:38:31 +0530

    Update index.js

--- Blog-Appwrite-Project/components/index.js
+++ Blog-Appwrite-Project/components/index.js
@@ -1,4 +1,16 @@
 import Header from "./Header/Header"
 import Footer from "./Footer/Footer"
+import Container from "./container/Container"
+import Logo from "./Logo"
+import LogoutBtn from "./Header/LogoutBtn"
+import Input from "./Input"
+import RTE from "./RTE"
+import PostForm from './post-form/PostForm'
+import PostCard from './PostCard'
+import AuthLayout from './AuthLayout'
+import Select from './Select'
+import Signup from './Signup'
+import Button from './Button'
+import Login from "./Login"
 
-export {Header,Footer}
+export {Header,Login,Footer,Container,Logo,LogoutBtn,Input,RTE,PostCard,PostForm,AuthLayout,Select,Signup,Button}
