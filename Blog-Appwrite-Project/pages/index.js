c60f700c07c1a187a0025d8bd732cdc2a0ed12da
Tushar Pawar <tushharpawar14@gmail.com>
2023-10-04 19:54:57 +0530

    Create index.js

---
+++ Blog-Appwrite-Project/pages/index.js
@@ -0,0 +1,17 @@
+import Home from "./Home";
+import Signup from './Signup'
+import AddPost from './AddPost'
+import AllPosts from "./AllPosts";
+import EditPost from "./EditPost";
+import Login from "./Login"
+import Post from "./Post";
+
+export {
+    Home,
+    Signup,
+    AddPost,
+    AllPosts,
+    EditPost,
+    Login,
+    Post
+}
