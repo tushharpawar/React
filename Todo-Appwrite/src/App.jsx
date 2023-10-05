6d3b85f33152cdbe34e674231e7001f73f3146b1
Tushar Pawar <tushharpawar14@gmail.com>
2023-10-05 22:28:32 +0530

    Create App.jsx

---
+++ Todo-Appwrite/src/App.jsx
@@ -0,0 +1,15 @@
+import Header from "./Header"
+import Input from './Input.jsx'
+
+function App() {
+  return (
+    <>
+      <div className="w-screen h-screen">
+          <Header/>
+          <Input/>
+      </div>
+    </>
+  )
+}
+
+export default App
