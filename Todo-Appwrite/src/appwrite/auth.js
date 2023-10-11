0c0c5b6e63f5df0851eedb68c99d953586080b25
Tushar Pawar <tushharpawar14@gmail.com>
2023-10-11 23:08:15 +0530

    Update auth.js

--- Todo-Appwrite/src/appwrite/auth.js
+++ Todo-Appwrite/src/appwrite/auth.js
@@ -1,74 +1,13 @@
 import conf from '../conf/conf'
-import { Client, Account ,ID} from 'appwrite';
+import { Client, Account ,Databases} from 'appwrite';
 
-export class AuthService{
-    client=new Client();
-    account;
+const client=new Client();
 
-    constructor(){
-        this.client
-        .setEndpoint('https://cloud.appwrite.io/v1')
-        .setProject(conf.appwriteProjectId);
+client.setEndpoint(conf.appwriteUrl)
+client.setProject(conf.appwriteProjectId)
 
-        this.account=new Account(this.client)
-    }
+export const account=new Account(client)
+export const databases=new Databases(client,conf.appwriteDatabaseId)
 
-    async createAccount(email,password,name){
-        try {
-            const userAccount= await this.account.create(ID.unique(),email,password,name);
 
-            if(userAccount){
-                //TODO:-------------
-                return this.login({email,password})
-            }
-            else{
-                   return userAccount
-            }
-        } catch (error) {
-            console.log("error:",error);
-        }
-    }
-
-    async login({email,password}){
-        try {
-         return await this.account.createEmailSession(email,password)
-        } catch (error) {
-            console.log("Error:",error);
-        }
-    }
-
-   async googleLogin(){
-        try {
-            return await this.account.createOAuth2Session(conf.appwriteGoogleUrl)
-        } catch (error) {
-            console.log(error);
-        }
-   }
-
-   async getUserFromGoogle(){
-       return await this.account.getSession('current')
-   }
-
-    async getCurrentUser(){
-        try{
-           return await this.account.get()
-        }
-        catch(error){
-            console.log(error);
-        }
-    }
-  
-    async logout(){
-        try{
-           return await this.account.deleteSessions()
-        }
-        catch(error){
-            console.log(console.error());
-        }
-    }
-
-}
-
-const authService=new AuthService()
-export default authService
 
