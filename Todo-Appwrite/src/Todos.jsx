b96d0a96fa1f05991c983c794158120ade75f87a
Tushar Pawar <tushharpawar14@gmail.com>
2023-10-12 23:23:31 +0530

    Update Todos.jsx

--- Todo-Appwrite/src/Todos.jsx
+++ Todo-Appwrite/src/Todos.jsx
@@ -1,13 +1,60 @@
+import { databases } from "./appwrite/auth";
+import { useState, useEffect } from "react";
+import conf from "./conf/conf";
+
+const Todos = () => {
+  const [todos, setTodos] = useState();
+
+  useEffect(() => {
+    const getTodos = databases.listDocuments(
+      conf.appwriteDatabaseId,
+      conf.appwriteCollectionId
+    );
+    getTodos.then(
+      function (response) {
+        setTodos(response.documents);
+      },
+      function (error) {
+        console.log(error);
+      }
+    );
+  }, []);
 
 
-const Todos = (todo) => {
+  const deleteTodo=(id)=>{
+        const promise=databases.deleteDocument(conf.appwriteDatabaseId,conf.appwriteCollectionId,id)
+        promise.then(
+          function(response){
+            console.log(response);
+          },
+          function(error){
+            console.log(error);
+          }
+        )
+             
+  }
 
- 
   return (
-    <div className='w-3/6 h-210 ml-80 mt-20 border-gray-400 '>
-          <p className="text-black text-lg">Your todo: {todo.title}</p>
-    </div>
-  )
-}
+    <>
+      <div>
+        {todos &&
+          todos.map((item) => (
+            <div key={item.$id}>
+              <div className="w-3/6 h-12 text-center p-3 border-black border-2 rounded-md text-xl ml-80 mt-20 bg-purple-400 flex justify-between items-center">
+                <div>
+                  <p className="text-black text-lg">{item.todos}</p>
+                </div>
+                <div>
+                  <span onClick={()=>{
+                    deleteTodo(item.$id)
+                  }} className=" text-red-700 cursor-pointer">Delete</span>
+                </div>
+              </div>
+            </div>
+          ))}
+      </div>
+    </>
+  );
+};
 
-export default Todos
+export default Todos;
