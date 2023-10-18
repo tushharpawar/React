7a6e1a10faa30c26031ce80144760fde81f49cd3
Tushar Pawar <tushharpawar14@gmail.com>
2023-10-18 19:55:44 +0530

    Update Discover.jsx

--- music-app/src/pages/Discover.jsx
+++ music-app/src/pages/Discover.jsx
@@ -1,3 +1,20 @@
-const Discover = () => <div>Discover</div>;
+import {Error,Loader,SongCard} from "../components/index"
+import {geners} from '../assets/constants'
+
+const Discover = () =>{
+    
+    return(
+          <>
+            <div className="flex flex-col ">
+               <div className="w-full flex justify-between items-center sm:flex-row flex-col mt-4 mb-10 ">
+               <h2 className="font-bold text-3xl text-white text-left">Discover</h2>
+                <select className="">
+
+                </select>
+               </div>
+          </div>
+          </>
+    )
+};
 
 export default Discover;
