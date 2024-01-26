a6380f54c71db0e769932e8674394b2c5df81e49
Tushar Pawar <tushharpawar14@gmail.com>
2024-01-26 22:34:00 +0530

    Create home.tsx

---
+++ E-Shop/ecommerce-frontend/src/pages/home.tsx
@@ -0,0 +1,33 @@
+import { Link } from 'react-router-dom'
+import ProductCard from '../components/product-card'
+
+const Home = () => {
+
+  const addToCartHandler = () => {
+
+  }
+  return (
+    <div className='home'>
+      <section>
+
+      </section>
+
+      <h1>Latest Products
+        <Link to="/search" className="findMore">More</Link>
+      </h1>
+
+      <main>
+        <ProductCard
+         productId='abcdef'
+         name='Macbook'
+         price={159999}
+         stock={15}
+         handler={addToCartHandler}
+         photo='https://m.media-amazon.com/images/I/71jG+e7roXL._SX569_.jpg'
+          />
+      </main>
+    </div>
+  )
+}
+
+export default Home
