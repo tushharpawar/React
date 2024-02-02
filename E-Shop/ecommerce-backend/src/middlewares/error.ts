3d64efffa0f6208613f3c0910321f784b66bab60
Tushar Pawar <tushharpawar14@gmail.com>
2024-02-02 22:02:01 +0530

    Create error.ts

---
+++ E-Shop/ecommerce-backend/src/middlewares/error.ts
@@ -0,0 +1,26 @@
+import {Request,NextFunction, Response } from "express"
+import ErrorHandler from "../utils/utility-class.js"
+import { ControllerType } from "../types/types.js"
+
+export const errorMiddleware = (err:ErrorHandler,req:Request,res:Response,next:NextFunction)=>{
+
+    err.message||="Internal server error"
+    err.statusCode||=500
+    
+    return res.status(400).json({
+        success:false,
+        message:err.message
+    })
+}
+
+export const TryCatch = (
+    func:ControllerType
+) => {
+    return (
+        req: Request,
+        res: Response,
+        next: NextFunction
+    ) => {
+        return Promise.resolve(func(req,res,next)).catch(next);
+    }
+}
