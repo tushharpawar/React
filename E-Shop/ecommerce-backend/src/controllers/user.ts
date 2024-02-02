dec6e91b363dea3eb9d1f1b86223ef9021558d8b
Tushar Pawar <tushharpawar14@gmail.com>
2024-02-02 22:01:46 +0530

    Update user.ts

--- E-Shop/ecommerce-backend/src/controllers/user.ts
+++ E-Shop/ecommerce-backend/src/controllers/user.ts
@@ -1,33 +1,77 @@
 import { Request, Response, NextFunction } from "express";
 import { User } from "../models/user.js";
 import { NewUserRequestBody } from "../types/types.js";
+import { TryCatch } from "../middlewares/error.js";
+import ErrorHandler from "../utils/utility-class.js";
 
-export const newUser =async (
-    req: Request<{},{},NewUserRequestBody>,
-    res: Response, 
+export const newUser = TryCatch(
+  async (
+    req: Request<{}, {}, NewUserRequestBody>,
+    res: Response,
     next: NextFunction
-    ) => {
-        try{
-            const {name,email,photo,_id,dob,gender}=req.body;
+  ) => {
+    const { name, email, photo, _id, dob, gender } = req.body;
 
-            const user = await User.create({
-                name,
-                email,
-                photo,
-                _id,
-                dob : new Date(dob),
-                gender,
-            })
+    let user = await User.findById(_id);
 
-            return res.status(201).json({
-                success:true,
-                message:`Welcome , ${user.name}`
-            });
-        }
-        catch(error){
-            return res.status(400).json({
-                success:false,
-                message:error,
-            });
-        }
-    };
+    if(user) return res.status(200).json({
+        success:true,
+        message:`Welcome , ${user.name}`,
+    })
+
+    if(!_id || !name || !photo || !gender || !email || !dob) 
+    return next(new ErrorHandler("Please add all fields!",400))
+
+    user = await User.create({
+      name,
+      email,
+      photo,
+      _id,
+      dob: new Date(dob),
+      gender,
+    });
+
+    return res.status(201).json({
+      success: true,
+      message: `Welcome , ${user.name}`,
+    });
+  }
+);
+
+
+export const getAllUsers = TryCatch(async (req,res,next) => {
+    const users = await User.find({})
+
+    return res.status(200).json({
+        success:true,
+        users,
+    })
+})
+
+export const getUser = TryCatch(async (req,res,next) => {
+    const id = req.params.id;
+    
+    const user = await User.findById(id);
+
+    if(!user) return next(new ErrorHandler("Invalid id",400));
+
+
+    return res.status(200).json({
+        success:true,
+        user,
+    })
+})
+
+export const deleteUser = TryCatch(async (req,res,next) => {
+    const id = req.params.id;
+    const user = await User.findById(id);
+
+    if(!user) return next(new ErrorHandler("Invalid id",400));
+    
+    await user.deleteOne();
+
+    return res.status(200).json({
+        success:true,
+        message:"User deleted successfully!",
+    })
+})
