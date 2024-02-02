72c814aa368fe3b45701158aad9d44b0e6ec0143
Tushar Pawar <tushharpawar14@gmail.com>
2024-02-02 22:02:17 +0530

    Update user.ts

--- E-Shop/ecommerce-backend/src/routes/user.ts
+++ E-Shop/ecommerce-backend/src/routes/user.ts
@@ -1,9 +1,16 @@
 import express from "express"
-import { newUser } from "../controllers/user.js";
+import { newUser,getAllUsers,getUser,deleteUser } from "../controllers/user.js";
+import { adminOnly } from "../middlewares/auth.js";
 
 const app = express.Router();
 
 //route - /api/v1/user/new 
 app.post("/new",newUser)
+
+//route - /api/v1/user/all 
+app.get("/all",adminOnly,getAllUsers)
+
+//route - /api/v1/user/dynamicID
+app.route("/:id").get(getUser).delete(adminOnly,deleteUser)
 
 export default app;
