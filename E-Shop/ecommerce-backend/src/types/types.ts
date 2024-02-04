cb0f0a534cf558dc47b0d63f4738ae2031f042cd
Tushar Pawar <tushharpawar14@gmail.com>
2024-02-04 21:58:34 +0530

    Update types.ts

--- E-Shop/ecommerce-backend/src/types/types.ts
+++ E-Shop/ecommerce-backend/src/types/types.ts
@@ -1,23 +1,42 @@
 import { Request, Response, NextFunction } from "express";
 
-export interface NewUserRequestBody{
-    name:string;
-    email:string;
-    photo:string;
-    gender:string;
-    _id:string;
-    dob:Date;
+export interface NewUserRequestBody {
+  name: string;
+  email: string;
+  photo: string;
+  gender: string;
+  _id: string;
+  dob: Date;
 }
 
-export interface NewProductRequestBody{
-    name:string;
-    category:string,
-    price:number,
-    stock:number,
+export interface NewProductRequestBody {
+  name: string;
+  category: string;
+  price: number;
+  stock: number;
 }
 
 export type ControllerType = (
-    req:Request,
-    res:Response,
-    next:NextFunction,
- ) => Promise<void | Response<any,Record<string,any>>>;
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => Promise<void | Response<any, Record<string, any>>>;
+
+export type SearchRequestQuery = {
+  search?: string;
+  price?: string;
+  category?: string;
+  sort?: string;
+  page?: string;
+};
+
+export interface BaseQuery {
+  name?: {
+    $regex: string;
+    $options: string;
+  };
+  price?: {
+    $lte: number;
+  };
+  category?: string;
+}
