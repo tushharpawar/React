4fe74f07a380b51c9570c2862d71b620153fa0bd
Tushar Pawar <tushharpawar14@gmail.com>
2024-02-07 23:00:59 +0530

    Create order.ts

---
+++ E-Shop/ecommerce-backend/src/routes/order.ts
@@ -0,0 +1,12 @@
+import express from "express"
+import { adminOnly } from "../middlewares/auth.js";
+import { newOrder } from "../controllers/order.js";
+
+const app = express.Router();
+
+
+app.post("/new",newOrder)
+
+
+
+export default app;
