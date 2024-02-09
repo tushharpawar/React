2ce30bb272e78ce952d475bc3c42581f002d8bab
Tushar Pawar <tushharpawar14@gmail.com>
2024-02-09 20:58:07 +0530

    Update features.ts

--- E-Shop/ecommerce-backend/src/utils/features.ts
+++ E-Shop/ecommerce-backend/src/utils/features.ts
@@ -1,7 +1,8 @@
 import mongoose from "mongoose"
-import { InvalidateCacheProps } from "../types/types.js"
+import { InvalidateCacheProps, OrderItemType } from "../types/types.js"
 import { Product } from "../models/product.js"
 import { myCache } from "../app.js"
+import ErrorHandler from "./utility-class.js"
 
 export const connectDB =(uri:string)=>{
     mongoose.connect(uri , {
@@ -37,4 +38,17 @@ export const invalidateCache =async ({
     if(admin){
 
     }
+}
+
+export const reduceStock =async (orderItems:OrderItemType[])=>{
+    for (let i = 0; i < orderItems.length; i++) {
+        const order = orderItems[i];
+        const product = await Product.findById(order.productId)
+
+        if(!product) throw new Error("Product not found")
+        product.stock -=order.quantity;
+
+        await product.save();
+    }
+ 
 }
