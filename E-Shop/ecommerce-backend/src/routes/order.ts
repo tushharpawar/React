37db021c6258bcb0db6fe29489778c339d187d83
Tushar Pawar <tushharpawar14@gmail.com>
2024-02-29 22:03:20 +0530

    Update order.ts

--- E-Shop/ecommerce-backend/src/routes/order.ts
+++ E-Shop/ecommerce-backend/src/routes/order.ts
@@ -1,12 +1,15 @@
 import express from "express"
 import { adminOnly } from "../middlewares/auth.js";
-import { newOrder,myOrders } from "../controllers/order.js";
+import { newOrder,myOrders, allOrders,getSingleOrder } from "../controllers/order.js";
 
 const app = express.Router();
 
 
 app.post("/new",newOrder)
 app.get("/my",myOrders)
+app.get("/all",allOrders)
+
+app.route("/:id").get(getSingleOrder);
 
 
 
