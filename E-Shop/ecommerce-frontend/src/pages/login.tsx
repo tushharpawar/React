9217ee7791f9aa0f09d08945390d6047a36301bb
Tushar Pawar <tushharpawar14@gmail.com>
2024-03-16 21:22:11 +0530

    Update login.tsx

--- E-Shop/ecommerce-frontend/src/pages/login.tsx
+++ E-Shop/ecommerce-frontend/src/pages/login.tsx
@@ -1,11 +1,30 @@
+import { GoogleAuthProvider, signInWithPopup } from "firebase/auth"
 import { useState } from "react"
+import toast from "react-hot-toast"
 import {FcGoogle} from "react-icons/fc"
+import { auth } from "../firebase"
 
 
 const Login = () => {
 
   const [gender,setGender]=useState("")
   const [date,setDate]=useState("")
+
+  const loginHandler = async() =>{
+    try {
+      const provider = new GoogleAuthProvider();
+      const { user } = await signInWithPopup(auth,provider);
+      console.log(user);
+      
+
+
+
+
+    } catch (error) {
+      toast.error("Invalid login")
+    }
+  }
+
   return (
     <div className="login">
     <main>
@@ -27,7 +46,7 @@ const Login = () => {
 
     <div>
       <p>Already Signed In Once</p>
-      <button>
+      <button onClick={loginHandler}>
         <FcGoogle /> <span>Sign in with Google</span>
       </button>
     </div>
