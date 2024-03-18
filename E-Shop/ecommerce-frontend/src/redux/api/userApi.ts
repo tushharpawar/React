6dfd240a325c70a374854995e022d94fc57cff27
Tushar Pawar <tushharpawar14@gmail.com>
2024-03-18 22:29:26 +0530

    Update userApi.ts

--- E-Shop/ecommerce-frontend/src/redux/api/userApi.ts
+++ E-Shop/ecommerce-frontend/src/redux/api/userApi.ts
@@ -1,15 +1,20 @@
-import {createApi,fetchBaseQuery} from "@reduxjs/toolkit/query/react"
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { server } from "../store";
+import { MessageResponse } from "../../types/api-types";
+import { User } from "../../types/types";
 
-
-export const userAPI = createApi({reducerPath:"userApi" ,
-        baseQuery:fetchBaseQuery({baseUrl:`${server}/api/v1/user/`}),
-        endpoints:(builder)=>({
-         login:builder.mutation<string,number>({ query: (user) => ({
-            url:"new",
-            method:"POST",
-            body:user
-         }),
-       }),
-     }),
+export const userAPI = createApi({
+  reducerPath: "userApi",
+  baseQuery: fetchBaseQuery({ baseUrl: `${server}/api/v1/user/` }),
+  endpoints: (builder) => ({
+    login: builder.mutation<MessageResponse, User>({
+      query: (user) => ({
+        url: "new",
+        method: "POST",
+        body: user,
+      }),
+    }),
+  }),
 });
+
+export const { useLoginMutation } = userAPI;
