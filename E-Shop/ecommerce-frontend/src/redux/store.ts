363f3a176da4c7480f579334e8d402f2e24bd3ad
Tushar Pawar <tushharpawar14@gmail.com>
2024-03-18 22:29:15 +0530

    Update store.ts

--- E-Shop/ecommerce-frontend/src/redux/store.ts
+++ E-Shop/ecommerce-frontend/src/redux/store.ts
@@ -1,7 +1,12 @@
 import {configureStore} from "@reduxjs/toolkit";
+import { userAPI } from "./api/userApi";
 
 export const server = "http://localhost:4000";
 
 export const store =configureStore({
-    reducer:{},
+    reducer:{
+        [ userAPI.reducerPath ] : userAPI.reducer,
+
+    },
+    middleware: (mid)=>[...mid(),userAPI.middleware],
 })
